7ad36b1a1e7518a1b012f7c0266203dcbb22f1c1
Hubert Taler <hubert.taler@blstream.com>
2016-12-12 10:52:48 +0100

    example for ToString.Fody and Bogus

---
+++ FodyExample8/FodyExample8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+
+namespace FodyExample8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Randomizer.Seed = new Random(DateTime.Now.Millisecond);
+            var testPersons = new Faker<UserData>(locale: "pl").RuleFor(u => u.Firstname, f => f.Name.FirstName())
+                .RuleFor(u => u.LastName, f => f.Name.LastName())
+                .RuleFor(u => u.Address, f => f.Address.StreetAddress())
+                .RuleFor(u => u.Login, f => f.Person.UserName)
+                .RuleFor(u => u.PhoneNumber, u => u.Phone.PhoneNumber())
+                .RuleFor(u => u.BirthDate, f => f.Person.DateOfBirth)
+                .RuleFor(u => u.Password, f => f.Random.Replace("#######"));
+
+            var persons = testPersons.Generate(20);
+            foreach (var current in persons)
+            {
+                Console.WriteLine(current);
+            }
+        }
+    }
+}
---
+++ FodyExample8/FodyExample8/UserData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FodyExample8
+{
+    [ToString]
+    public class UserData
+    {
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Login { get; set; }
+        public DateTime BirthDate { get; set; }
+        [IgnoreDuringToString]
+        public string Password { get; set; }
+    }
+}

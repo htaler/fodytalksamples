d1acf5fad3290691af8cbd718a504b2475daa00d
Hubert Taler <hubert.taler@blstream.com>
2016-12-12 10:25:45 +0100

    new example for Mixin

---
+++ FodyExample7/FodyExample7/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mixins;
+
+namespace FodyExample7
+{
+    [Mixin(typeof(CommonTools))]
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ FodyExample7/FodyExample7/CommonTools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FodyExample7
+{
+    public interface ICommonTools
+    {
+        void OutputCurrentDate();
+    }
+    public class CommonTools : ICommonTools
+    {
+        public void OutputCurrentDate()
+        {
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+        }
+    }
+}
